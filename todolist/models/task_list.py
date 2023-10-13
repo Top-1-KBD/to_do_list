f854579c98f70b7afca1afbaf1578b82bbf3dd13
ErwanR92 <elyes.rezzoug@telecom-paris.fr>
2023-10-13 23:46:28 +0200

    Adding pipeline CI/CD, PEP8, docstring & coverage features

--- todolist/models/task_list.py
+++ todolist/models/task_list.py
@@ -1,16 +1,36 @@
+"""Module for managing tasks in a task list."""
+
 from typing import List
 from todolist.models.task import Task
-from todolist.exceptions.task_exceptions import TaskNotFoundError 
+from todolist.exceptions.task_exceptions import TaskNotFoundError
+
 
 class TaskList:
+    """A class to represent a list of tasks."""
+
     def __init__(self):
+        """Initialize an empty list of tasks."""
         self.tasks: List[Task] = []
 
     def add_task(self, name: str, description: str) -> None:
+        """Add a new task to the list.
+
+        Args:
+            name (str): The name of the task.
+            description (str): A description of the task.
+        """
         task = Task(name, description)
         self.tasks.append(task)
 
     def complete_task(self, task_name: str) -> None:
+        """Mark a task as complete.
+
+        Args:
+            task_name (str): The name of the task to mark as complete.
+
+        Raises:
+            TaskNotFoundError: If the task with the given name is not found.
+        """
         for task in self.tasks:
             if task.name == task_name:
                 task.mark_as_complete()
@@ -18,6 +38,14 @@ class TaskList:
         raise TaskNotFoundError("Task not found!")
 
     def remove_task(self, task_name: str) -> None:
+        """Remove a task from the list.
+
+        Args:
+            task_name (str): The name of the task to remove.
+
+        Raises:
+            TaskNotFoundError: If the task with the given name is not found.
+        """
         for task in self.tasks:
             if task.name == task_name:
                 self.tasks.remove(task)
@@ -25,4 +53,15 @@ class TaskList:
         raise TaskNotFoundError("Task not found!")
 
     def display_tasks(self) -> List[str]:
-        return [f"{task.name} - {task.description}" for task in self.tasks if not task.completed]
+        """Return a list of uncompleted tasks.
+
+        with their names and descriptions.
+
+        Returns:
+            List[str]: A list of uncompleted tasks.
+        """
+        return [
+            f"{task.name} - {task.description}"
+            for task in self.tasks
+            if not task.completed
+        ]
--- todolist/todolist/logger/log_config.py
+++ todolist/todolist/logger/log_config.py
@@ -1,11 +1,20 @@
+"""This module provides configuration utilities.
+
+for logging within the todolist application.
+"""
+
 import logging
 
+
 def setup_logger():
+    """Set up and configure the debug and error loggers for the application."""
     # Logger pour debug
     debug_logger = logging.getLogger("debug_logger")
     debug_logger.setLevel(logging.DEBUG)
     debug_fh = logging.FileHandler("debug.log")
-    debug_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    debug_format = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
     debug_fh.setFormatter(debug_format)
     debug_logger.addHandler(debug_fh)
 
@@ -13,8 +22,11 @@ def setup_logger():
     error_logger = logging.getLogger("error_logger")
     error_logger.setLevel(logging.ERROR)
     error_fh = logging.FileHandler("error.log")
-    error_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    error_format = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
     error_fh.setFormatter(error_format)
     error_logger.addHandler(error_fh)
+
 
 setup_logger()

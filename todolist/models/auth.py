d532916de4b9d6ecd239de43e1490d157cc0afcf
ErwanR92 <elyes.rezzoug@telecom-paris.fr>
2023-10-21 00:50:49 +0200

    authentification test

---
+++ todolist/models/auth.py
@@ -0,0 +1,17 @@
+from models.utilisateurs import User, users_db
+
+def register(username, password):
+    if username in users_db:
+        raise ValueError("User already exists")
+    user = User(username, password)
+    users_db[username] = user
+
+def login(username, password):
+    user = users_db.get(username)
+    if user and user.check_password(password):
+        return {"status": "logged in", "username": username}
+    else:
+        return {"status": "failed"}
+
+def logout(session):
+    session.clear()  # Simule un dictionnaire de session

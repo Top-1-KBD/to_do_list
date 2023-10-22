f3a11b0c8da51dd67d0f7143b035164bc2cb6664
ErwanR92 <elyes.rezzoug@telecom-paris.fr>
2023-10-22 20:50:37 +0200

    auth dosctring

--- todolist/models/auth.py
+++ todolist/models/auth.py
@@ -31,7 +31,7 @@ def register(username, password):
 
 def login(username, password):
     """Log in a user with the given username and password.
-    
+
     Args:
         username (str): The username of the user attempting to log in.
         password (str): The password provided by the user for authentication.

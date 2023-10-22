c5627af81ddb6a0c751c691e2d4ffa8755f7ee36
ErwanR92 <elyes.rezzoug@telecom-paris.fr>
2023-10-22 13:55:59 +0200

    PEP8 modif

--- todolist/models/auth.py
+++ todolist/models/auth.py
@@ -1,10 +1,12 @@
 from models.utilisateurs import User, users_db
 
+
 def register(username, password):
     if username in users_db:
         raise ValueError("User already exists")
     user = User(username, password)
     users_db[username] = user
+
 
 def login(username, password):
     user = users_db.get(username)
@@ -12,6 +14,7 @@ def login(username, password):
         return {"status": "logged in", "username": username}
     else:
         return {"status": "failed"}
+
 
 def logout(session):
     session.clear()  # Simule un dictionnaire de session

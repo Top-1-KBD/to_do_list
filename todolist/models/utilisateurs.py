d02fc172ef6c65cd64eb59aa27d35a708b0f15f7
ErwanR92 <elyes.rezzoug@telecom-paris.fr>
2023-10-22 14:20:16 +0200

    roles add

--- todolist/models/utilisateurs.py
+++ todolist/models/utilisateurs.py
@@ -1,15 +1,17 @@
 """Module for user-related classes and functions."""
 
 from werkzeug.security import generate_password_hash, check_password_hash
+from .roles import Role
 
 
 class User:
     """Class representing a user."""
 
-    def __init__(self, username, password):
-        """Initialize a new user with the given username and password."""
+    def __init__(self, username, password, role=Role.USER):
+        """Initialize a new user with the given username, password, and role."""
         self.username = username
         self.password_hash = generate_password_hash(password)
+        self.role = role
 
     def check_password(self, password):
         """Check if the given password matches the user's password."""

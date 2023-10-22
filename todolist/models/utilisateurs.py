9ca750cc6a6d892f8967f45d5570ad8fe4b41941
ErwanR92 <elyes.rezzoug@telecom-paris.fr>
2023-10-22 18:28:44 +0200

    utilisateur flake8

--- todolist/models/utilisateurs.py
+++ todolist/models/utilisateurs.py
@@ -13,7 +13,6 @@ from .database import add_user, get_user
 class User:
     """Class representing a user."""
 
-
     def __init__(self, username, password, role=Role.USER):
         """Init a new user with the given username, password, and role."""
         self.username = username
@@ -22,7 +21,6 @@ class User:
         # Ajoutez l'utilisateur à la base de données
         if not add_user(username, password, role):
             raise ValueError(f"Nom d'utilisateur '{username}' déjà pris")
-
 
     @classmethod
     def fetch_user(cls, username):
@@ -35,7 +33,6 @@ class User:
             user.password_hash = password_hash
             return user
         return None
-
 
     def check_password(self, password):
         """Check if the given password matches the user's password."""

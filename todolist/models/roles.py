7010ea16d8396f766ac4eb2e5f6c4641e0b4796b
ErwanR92 <elyes.rezzoug@telecom-paris.fr>
2023-10-23 10:08:37 +0200

    delete files

--- todolist/models/roles.py
+++
@@ -1,8 +0,0 @@
-"""Module defining the roles for users in the to-do list application."""
-
-
-class Role:
-    """Enum representing the various roles a user can have."""
-
-    ADMIN = "admin"
-    USER = "user"

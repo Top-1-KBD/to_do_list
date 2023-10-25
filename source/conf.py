7cc6ccad9624e2fdb191892b6e35eb7ff649af59
ErwanR92 <elyes.rezzoug@telecom-paris.fr>
2023-10-25 11:33:05 +0200

    update setup

--- source/conf.py
+++ source/conf.py
@@ -1,3 +1,10 @@
+"""Configuration file for Sphinx documentation.
+
+This file only contains a selection of the most common options. For a full
+list see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:

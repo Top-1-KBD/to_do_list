09df1219e160fad52c0694855dbeff6390ac3477
ErwanR92 <elyes.rezzoug@telecom-paris.fr>
2023-10-25 21:52:17 +0200

    docstring test task

--- todolist/tests/test_task.py
+++ todolist/tests/test_task.py
@@ -2,12 +2,14 @@
 
 from todolist.models.task import Task
 
+
 def test_task_creation():
     """Test the creation of a task."""
     task = Task("Test Task", "A simple task")
     assert task.name == "Test Task"
     assert task.description == "A simple task"
     assert not task.completed
+
 
 def test_mark_as_complete():
     """Test marking a task as complete."""

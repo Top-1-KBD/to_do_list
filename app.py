38aa672441ea10fbe0678b0755bc848b696157c0
Rayann237 <rtchokogoue@yahoo.fr>
2023-10-30 22:03:36 +0100

    Add case for task management

--- app.py
+++ app.py
@@ -28,9 +28,10 @@ while CHOICE != "3":
             print("What do you want to do?")
             print("1. Create a new task")
             print("2. List all your tasks")
-            print("3. Change a task status")
-            print("4. Delete a task")
-            print("5. Return to the previous menu")
+            print("3. Assign a task")
+            print("4. Change a task status")
+            print("5. Delete a task")
+            print("6. Return to the previous menu")
             choice_action = input("Enter your choice: ")
 
             if choice_action == "1":
@@ -39,8 +40,10 @@ while CHOICE != "3":
             elif choice_action == "2":
                 print("You selected 'List all your tasks'")
             elif choice_action == "3":
-                print("You selected 'Change a task status'")
+                print("You selected 'Assign a task'")
             elif choice_action == "4":
+                print("You selected 'Change a task status'")
+            elif choice_action == "5":
                 print("You selected 'Delete a task'")
 
         else:
